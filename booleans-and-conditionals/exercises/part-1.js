f6f8f680a6ecdade18f3d912acc41bf8ac706df9
Kyle McColgan <kdmbus21@gmail.com>
2024-01-08 19:23:01 -0600

    Complete the conditional statements.

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,26 @@
+//****************************************************************************************
+//
+//
+//    Filename:    part-1.js
+//    Date:        8 January 2024
+//    Author:      Kyle McColgan
+//    Description: This program performs some basic JavaScript operations using conditionals.
+//
+//
+//****************************************************************************************
+
 // Declare and initialize the variables for exercise 1 here:
 
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+
+//engines are off, the first two conditional checks fail because engineIndicatorLight is assigned red blinking, not green.
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -9,3 +29,6 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+//****************************************************************************************
+//engines are off
--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -1,24 +1,76 @@
-let engineIndicatorLight = 'red blinking';
-let fuelLevel = 21000;
-let engineTemperature = 1200;
+//****************************************************************************************
+//
+//
+//    Filename:    part-3.js
+//    Date:        8 January 2024
+//    Author:      Kyle McColgan
+//    Description: This program performs some basic JavaScript operations using conditionals.
+//
+//
+//****************************************************************************************
 
-/* 5) Implement the following checks using if/else if/else statements:
+let engineIndicatorLight = 'NOT red blinking';
+let fuelLevel = 18000;
+let engineTemperature = 2500;
 
-a) If fuelLevel is above 20000 AND engineTemperature is at or below 2500, print "Full tank. Engines good."
+// 5) Implement the following checks using if/else if/else statements:
 
-b) If fuelLevel is above 10000 AND engineTemperature is at or below 2500, print "Fuel level above 50%.  Engines good."
+const lowFuelLevel = 1000;
+const mediumFuelLevel = 5000;
+const highFuelLevel = 10000;
+const fullFuelLevel = 20000;
 
-c) If fuelLevel is above 5000 AND engineTemperature is at or below 2500, print "Fuel level above 25%. Engines good."
+const highEngineTemp = 3500;
+const avgEngineTemp = 2500;
 
-d) If fuelLevel is at or below 5000 OR engineTemperature is above 2500, print "Check fuel level. Engines running hot."
+const errString = "red blinking";
 
-e) If fuelLevel is below 1000 OR engineTemperature is above 3500 OR engineIndicatorLight is red blinking print "ENGINE FAILURE IMMINENT!" 
-
-f) Otherwise, print "Fuel and engine status pending..." */
+//a) If fuelLevel is above 20000 AND engineTemperature is at or below 2500, print "Full tank. Engines good."
+if ( (fuelLevel <= lowFuelLevel) || (engineTemperature > highEngineTemp) || (engineIndicatorLight === errString))
+{
+    console.log("ENGINE FAILURE IMMINENT!");
+}
+else if ( (fuelLevel <= mediumFuelLevel) || (engineTemperature > avgEngineTemp))
+{
+    console.log("Check fuel level. Engines running hot.");
+}
+else if ( (fuelLevel > fullFuelLevel) && (engineTemperature <= avgEngineTemp))
+{
+    console.log("Full tank. Engines good.");
+}
+else if ( (fuelLevel > highFuelLevel) && (engineTemperature <= avgEngineTemp))
+{
+    console.log("Fuel level above 50%. Engines good.");
+}
+else if ( (fuelLevel > mediumFuelLevel) && (engineTemperature <= avgEngineTemp))
+{
+    console.log("Fuel level above 25%. Engines good.");
+}
+else
+{
+    console.log("Fuel and engine status pending...");
+}
 
 // Code 5a - 5f here:
 
 // 6) a) Create the variable commandOverride, and set it to be true or false. If commandOverride is false, then the shuttle should only launch if the fuel and engine check are OK. If commandOverride is true, then the shuttle will launch regardless of the fuel and engine status.
+let commandOverride = true;
+
 
 /* 6) b) Code the following if/else check:
 If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
+
+if ( ((fuelLevel > fullFuelLevel) && (engineIndicatorLight !== 'NOT red blinking')) || (commandOverride === true))
+{
+    console.log("Cleared to launch!");
+}
+else
+{
+    console.log("Launch scrubbed!");
+}
+
+//****************************************************************************************
+//node part-3.js
+//Full level above 50%. Engines good.
+//Cleared to launch!
+
