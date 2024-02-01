c427736e6f4a38bc17dd535510d95c852f43be39
Kyle McColgan <kdmbus21@gmail.com>
2024-01-31 21:28:33 -0600

    Upload chapter eleven textbook examples.

--- more-on-functions/chapter-examples/recursion-base-case/factorial.js
+++ more-on-functions/chapter-examples/recursion-base-case/factorial.js
@@ -1,13 +1,65 @@
+//****************************************************************************************
+//
+//    Filename:    factorial.js
+//    Date:        29 January 2024
+//    Author:      Kyle McColgan
+//    Description: This program computes the factorial of an whole positive integer
+//                 value using recursion.
+//
+//****************************************************************************************
+
 //The following concept check assumes that only positive integers are passed to the function.
 
-function factorial(integer){
-  if (/*enter base case test here*/){
-    return integer;
-  } else {
-    return integer*(factorial(integer-1));
-  }
+//****************************************************************************************
+
+function factorial(integer)
+{
+    let result = integer;
+
+    if ( typeof integer == "string")
+    {
+        result = "Error: input cannot be a string value...";
+    }
+    else if (integer %2 !== 0 && integer %2 !== 1)
+    {
+        result = "Error: input cannot be a floating point value...";
+    }
+    else if (integer < 0)
+    {
+        result = "Error: input must be a positive integer value...";
+    }
+    else
+    {
+        if (integer === 1)
+        {
+            result = integer;
+        }
+        else
+        {
+            result = integer*(factorial(integer-1));
+        }
+    }
+
+    return result;
 }
 
+//****************************************************************************************
+
 console.log(factorial(4));
+console.log(factorial("4"));
+console.log(factorial(-4));
+console.log(factorial(3.01));
+
+//****************************************************************************************
 
 //Skill boost! Add validation to return an error message if the function is passed a string, negative number or a decimal value.
+
+//****************************************************************************************
+
+/*
+recursion-base-case> node factorial.js
+24
+Error: input cannot be a string value...
+Error: input must be a positive integer value...
+Error: input cannot be a floating point value...
+*/
--- more-on-functions/chapter-examples/recursion-base-case/removeI.js
+++ more-on-functions/chapter-examples/recursion-base-case/removeI.js
@@ -1,5 +1,14 @@
+//****************************************************************************************
+//
+//    Filename:    removeI.js
+//    Date:        29 January 2024
+//    Author:      Kyle McColgan
+//    Description: This program removes all elements with value = 'i' from an array.
+//
+//****************************************************************************************
+
 function removeI(arr) {
-    if (/* Enter base case test here */){
+    if (arr.indexOf('i') < 1){
       return arr;
     } else {
       arr.splice(arr.indexOf('i'),1);
@@ -7,6 +16,22 @@ function removeI(arr) {
     }
 };
 
-let arrayToChange = ['One', 'i', 'c', 'X', 'i', 'i', 54];
+//****************************************************************************************
 
+let arrayToChange = ['One', 'i', 'c', 'X', 'i', 'i', 54];
+console.log(`Starting array...${arrayToChange}`);
+
+console.log("Removing all elements with value = 'i'");
+console.log("Updated array...");
 console.log(removeI(arrayToChange));
+
+//****************************************************************************************
+
+/*
+recursion-base-case> node removeI.js
+Starting array...One,i,c,X,i,i,54
+Removing all elements with value = 'i'
+Updated array...
+[ 'One', 'c', 'X', 54 ]
+*/
+
