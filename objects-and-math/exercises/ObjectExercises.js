0ad7e3d6052218caae6cda3c9efd3b2ee38a11b0
Kyle McColgan <kdmbus21@gmail.com>
2024-02-01 18:19:12 -0600

    Upload completed chapter twelve exercise with Objects.

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -1,83 +1,83 @@
 //****************************************************************************************
 //
 //    Filename:    ObjectExercises.js
-//    Date:        31 January 2024
+//    Date:        1 February 2024
 //    Author:      Kyle McColgan
 //    Description: This program performs an animal race using Objects in JavaScript.
 //
 //****************************************************************************************
 
 let superChimpOne = {
-   name: "Chad",
-   species: "Chimpanzee",
-   mass: 9,
-   age: 6,
-   astronautID: 1,
+    name: "Chad",
+    species: "Chimpanzee",
+    mass: 9,
+    age: 6,
+    astronautID: 1,
 
-   move: function()
-   {
-      return Math.random()*10;
-   }
+    move: function()
+    {
+        return Math.ceil(Math.random()*10);
+    }
 };
 
 //****************************************************************************************
 
 let superChimpTwo = {
-   name: "Brad",
-   species: "Chimpanzee",
-   mass: 11,
-   age: 6,
-   astronautID: 2,
+    name: "Brad",
+    species: "Chimpanzee",
+    mass: 11,
+    age: 6,
+    astronautID: 2,
 
-   move: function()
-   {
-      return Math.random()*10;
-   }
+    move: function()
+    {
+        return Math.ceil(Math.random()*10);
+    }
 };
 
 //****************************************************************************************
 
 let superBeagle = {
-   name: "Leroy",
-   species: "Beagle",
-   mass: 14,
-   age: 5,
-   astronautID: 3,
+    name: "Leroy",
+    species: "Beagle",
+    mass: 14,
+    age: 5,
+    astronautID: 3,
 
-   move: function()
-   {
-      return Math.random()*10;
-   }
+    move: function()
+    {
+        return Math.ceil(Math.random()*10);
+    }
 };
 
 //****************************************************************************************
 
 let superTardigrade = {
-   name: "Almina",
-   species: "Tardigrade",
-   mass: 0.0000000001,
-   age: 1,
-   astronautID: 4,
+    name: "Almina",
+    species: "Tardigrade",
+    mass: 0.0000000001,
+    age: 1,
+    astronautID: 4,
 
-   move: function()
-   {
-      return Math.random()*10;
-   }
+    move: function()
+    {
+        return Math.ceil(Math.random()*10);
+    }
 };
 
 //****************************************************************************************
 
 let salamander = {
-   name: "Lacey",
-   species: "Axolotl Salamander",
-   mass: 0.1,
-   age: 5,
-   astronautID: 10,
+    name: "Lacey",
+    species: "Axolotl Salamander",
+    mass: 0.1,
+    age: 5,
+    astronautID: 10,
 
-   move: function()
-   {
-      return Math.random()*10;
-   }
+    move: function()
+    {
+        return Math.ceil(Math.random()*10);
+    }
 };
 
 //****************************************************************************************
@@ -149,17 +149,49 @@ function fitnessTest(arr)
 // Start an animal race!
 
 let crew = [superChimpOne,superChimpTwo,superBeagle,superTardigrade,salamander];
+
+console.log("\nStarting crew reports...");
+
+for ( animal in crew)
+{
+    console.log(crewReports(animal));
+}
+
+console.log("\nDone.");
+console.log("\nStarting the fitness test...");
 console.log(fitnessTest(crew));
+console.log("\nDone.");
+console.log("\nGoodbye!");
 
 //****************************************************************************************
 
 /*
 exercises> node ObjectExercises.js
+
+Starting crew reports...
+undefined is a undefined. They are undefined years old and
+            undefined kilograms. Their ID is undefined.
+undefined is a undefined. They are undefined years old and
+            undefined kilograms. Their ID is undefined.
+undefined is a undefined. They are undefined years old and
+            undefined kilograms. Their ID is undefined.
+undefined is a undefined. They are undefined years old and
+            undefined kilograms. Their ID is undefined.
+undefined is a undefined. They are undefined years old and
+            undefined kilograms. Their ID is undefined.
+
+Done.
+
+Starting the fitness test...
 [
-  'Chad took 7 turns to take 20 steps.',
-  'Brad took 3 turns to take 20 steps.',
+  'Chad took 6 turns to take 20 steps.',
+  'Brad took 5 turns to take 20 steps.',
   'Leroy took 4 turns to take 20 steps.',
-  'Almina took 6 turns to take 20 steps.',
+  'Almina took 5 turns to take 20 steps.',
   'Lacey took 4 turns to take 20 steps.'
 ]
+
+Done.
+
+Goodbye!
 */
