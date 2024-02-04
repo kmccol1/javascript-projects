c9660bd895acd714cb380298aeb37d07b3f48685
Kyle McColgan <kdmbus21@gmail.com>
2024-02-04 01:44:35 -0600

    Upload chapter fourteen textbook example exercises.

---
+++ unit-testing/chapter-examples/transmission-processor/processor.js
@@ -0,0 +1,30 @@
+//****************************************************************************************
+//
+//    Filename:    processor.js
+//    Date:        4 February 2024
+//    Author:      Kyle McColgan
+//    Description: This program tests the function isPalindrome().
+//
+//****************************************************************************************
+
+function processor(transmission)
+{
+    if (transmission.indexOf("::") < 0)
+    {
+    // Data is invalid
+        return -1;
+    }
+    let parts = transmission.split("::");
+    let rawData = parts[1];
+    if (rawData[0] !== "<")
+    {
+        rawData = -1;
+    }
+
+return {
+id: Number(parts[0]),
+rawData: rawData
+};
+}
+
+module.exports = processor;
