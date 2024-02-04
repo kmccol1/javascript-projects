e82c6488a7cd27f32607fc7c67e8f8223c9f7efe
Kyle McColgan <kdmbus21@gmail.com>
2024-02-03 19:37:53 -0600

    Upload remaining chapter twelve textbook examples.

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -1,3 +1,12 @@
+//****************************************************************************************
+//
+//    Filename:    ForInLoop.js
+//    Date:        3 February 2024
+//    Author:      Kyle McColgan
+//    Description: This program uses a for..in loop to display an Object's properties.
+//
+//****************************************************************************************
+
 let tortoiseOne = {
    species: "Galapagos Tortoise",
    name: "Pete",
@@ -6,4 +15,22 @@ let tortoiseOne = {
    diet: ["pumpkins", "lettuce", "cabbage"]
 };
 
+//****************************************************************************************
+
+for (key in tortoiseOne)
+{
+    console.log(`${key} : ${tortoiseOne[key]}`);
+}
+
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+
+//****************************************************************************************
+
+/*
+chapter-examples> node ForInLoop.js
+species : Galapagos Tortoise
+name : Pete
+weight : 919
+age : 85
+diet : pumpkins,lettuce,cabbage
+*/
--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -1,3 +1,12 @@
+//****************************************************************************************
+//
+//    Filename:    KindnessSelection.js
+//    Date:        3 February 2024
+//    Author:      Kyle McColgan
+//    Description: This program uses a for..in loop to display an Object's properties.
+//
+//****************************************************************************************
+
 function randomSelection(arr){
    let index = Math.floor(Math.random()*arr.length);
    return arr[index];
@@ -10,8 +19,81 @@ function randomSelection(arr){
  for (i=0; i < 8; i++){
    console.log(randomSelection(happiness));
  }
+
+//****************************************************************************************
+
  
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
+console.log("\nPrinting three random selections from words and happiness arrays: ");
+
+for(let i = 0; i < 3; i ++ )
+{
+    console.log(`${i} : ${randomSelection(happiness)}`);
+}
+
+for(let i = 0; i < 3; i ++ )
+{
+    console.log(`${i} : ${randomSelection(words)}`);
+}
  //b) Have the code randomly pick one array, and then print 2 random items from it.
- //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+
+console.log("\nPrinting two randomly picked items from a randomly picked array: ");
+let randomArrayNum = Math.random()*10;
+
+if (randomArrayNum % 2 == 0)
+{
+    for(let i = 0; i < 2; i ++ )
+    {
+        console.log(`${i} : ${randomSelection(happiness)}`);
+    }
+}
+else
+{
+    for(let i = 0; i < 2; i ++ )
+    {
+        console.log(`${i} : ${randomSelection(words)}`);
+    }
+}
+//c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+
+let newArr = [];
+
+newArr.push(randomSelection(words));
+newArr.push(randomSelection(happiness));
+
+console.log("\nPrinting two randomly picked items from both arrays: ");
+
+for(let i = 0; i < newArr.length; i ++ )
+{
+    console.log(`${i} : ${newArr[i]}`);
+}
+//****************************************************************************************
+
+/*
+chapter-examples> node KindnessSelection.js
+Joy
+Kindness
+Peace
+Kittens
+Peace
+Kindness
+Joy
+Kindness
+
+Printing three random selections from words and happiness arrays:
+0 : Puppies
+1 : Love
+2 : Kittens
+0 : Hello
+1 : Hello
+2 : Python
+
+Printing two randomly picked items from a randomly picked array:
+0 : JavaScript
+1 : Hello
+
+Printing two randomly picked items from both arrays:
+0 : Rutabaga
+1 : Kittens
+*/
