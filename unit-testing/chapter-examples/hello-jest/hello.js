46e9057118c7017adb3b80577212762c7995156e
Kyle McColgan <kdmbus21@gmail.com>
2024-02-03 22:40:57 -0600

    Upload Jest Hello, World! solution.

--- unit-testing/chapter-examples/hello-jest/hello.js
+++ unit-testing/chapter-examples/hello-jest/hello.js
@@ -1,8 +1,20 @@
+//****************************************************************************************
+//
+//    Filename:    hello.js
+//    Date:        3 February 2024
+//    Author:      Kyle McColgan
+//    Description: This program is a simple "Hello, World!" project built using Jest for
+//                 unit testing purposes.
+//
+//****************************************************************************************
+
 function hello(name) {
    if (name === undefined)
        name = "World";
 
    return "Hello, " + name + "!";
 }
+
+//****************************************************************************************
 
 module.exports = hello;
