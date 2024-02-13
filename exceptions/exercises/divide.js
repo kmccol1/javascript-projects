3ff8784b51d745df4ae45bfee6671f94be555e90
Kyle McColgan <kdmbus21@gmail.com>
2024-02-12 18:46:24 -0600

    Upload chapter seventeen textbook exercises.

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,47 @@
+//****************************************************************************************
+//
+//    Filename:    divide.js
+//    Date:        12 February 2024
+//    Author:      Kyle McColgan
+//    Description: This program provides a function to divide two numbers and throws
+//                 an exception if dividing by zero.
+//
+//****************************************************************************************
+
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+function divide (num, denom)
+{
+    let result;
+
+    if (denom == 0)
+    {
+        throw new Error("Error: You cannot divide by zero!");
+    }
+    else
+    {
+        result = num / denom;
+    }
+
+    return result;
+}
+
+//****************************************************************************************
 
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+//****************************************************************************************
+
+console.log("6/7=",divide(6,7));
+console.log("21/0=",divide(21,0));
+
+//****************************************************************************************
+
+/*
+divide.js:18
+        throw new Error("Error: You cannot divide by zero!");
+        ^
+*/
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,10 +1,35 @@
-function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+//****************************************************************************************
+//
+//    Filename:    test-student-labs.js
+//    Date:        12 February 2024
+//    Author:      Kyle McColgan
+//    Description: This program provides a function to grade lab scores and throws
+//                 an exception if a method does not yet exist in the objects.
+//
+//****************************************************************************************
+
+function gradeLabs(labs)
+{
+    let result;
+    for (let i=0; i < labs.length; i++)
+    {
+        let lab = labs[i];
+        try
+        {
+            result = lab.runLab(3);
+        }
+        catch(err)
+        {
+            console.log("We caught a TypeError, but our program continues to run!");
+            console.log("You tried to access the runLab method!");
+            result = "Error thrown";
+        }
+
+        console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
+
+//****************************************************************************************
 
 let studentLabs = [
   {
@@ -22,3 +47,39 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
+
+//****************************************************************************************
+
+/*
+exercises> node test-student-labs.js
+Carly code worked: true
+Erica code worked: false
+We caught a TypeError, but our program continues to run!
+You tried to access the runLab method!
+Blake code worked: false
+Jessica code worked: true
+Mya code worked: false
+*/
