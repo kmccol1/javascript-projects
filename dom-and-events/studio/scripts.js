7cffb7bee22ac8a7c6556e01621f19d29810d8bb
Kyle McColgan <kdmbus21@gmail.com>
2024-02-26 23:56:37 -0600

    Upload chapter twenty three studio code.

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,10 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function()
+{
+    let takeOff = document.getElementById("takeoff");
+    takeOff.addEventListener("click", function(){
+        window.confirm("Confirm that the shuttle is ready for takeoff.")
+    })
+}
+)
