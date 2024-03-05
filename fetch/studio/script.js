6319f45f635ff74a12c21959477f4f8acc8ab161
Kyle McColgan <kdmbus21@gmail.com>
2024-03-04 20:37:01 -0600

    Upload chapter twenty seven fetch studio code.

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -4,7 +4,23 @@ window.addEventListener("load", function(){
     //form.addEventListener("submit", function(event){
     //event.preventDefault();
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        let astronautDiv;
+        let bioDiv;
+        let container = document.getElementById('container');
+        for(let i = 0; i < response.length; i ++ )
+        {
+            const firstName = response[i].firstName;
+            const lastName = response[i].lastName;
+            astronautDiv = document.createElement('div');
+            astronautDiv.className = 'astronaut';
+            bioDiv = document.createElement('div');
+            bioDiv.className = 'bio';
+            bioDiv.textContent = 'This is the astronaut\'s bio.';
+            astronautDiv.appendChild(bioDiv);
+            container.appendChild(astronautDiv);
+        }
         console.log(response);
+
     }
 
 
