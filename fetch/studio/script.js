5f4bcb79ffb73c6b896e3d9c1af362c8af2c2b54
Kyle McColgan <kdmbus21@gmail.com>
2024-03-04 19:28:56 -0600

    Upload studio twenty seven script.js inital code.

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,13 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    //let form = document.getElementById('searchForm');
+    //form.addEventListener("submit", function(event){
+    //event.preventDefault();
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        console.log(response);
+    }
+
+
+
+
+    )});
